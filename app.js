c8fea27abe786d3eb61667ae550ae70f7455b65a
checkiamsiam <issiam02415@gmail.com>
2022-02-28 23:10:19 +0600

    more works

--- app.js
+++ app.js
@@ -9,21 +9,28 @@ document.getElementById('button-addon2').addEventListener('click', function () {
 })
 
 const displayResult = phones => {
-  searchText.value = '';
-  resultContainer.textContent = '';
-  for (let i = 0 ; i < 20 ; i++){
-    console.log(phones.data[i]);
-    const result = document.createElement('div');
-    result.classList.add('col');
-    result.innerHTML = `
-            <div class="card h-100">
-              <img src="${phones.data[i].image}" class="card-img-top w-75 m-auto mt-2" alt="image didn't find" />
-              <div class="card-body">
-                <h5 class="card-title">${phones.data[i].phone_name}</h5>
-                <h6 class="card-title">${phones.data[i].brand}</h6>
+
+  if (searchText.value == '') {
+    alert('dddd')
+  }else if (phones.data.length == 0) {
+    alert('ssss')
+  }else {
+    searchText.value = '';
+    resultContainer.textContent = '';
+    for (let i = 0; i < 20; i++) {
+      console.log(phones.data[i]);
+      const result = document.createElement('div');
+      result.classList.add('col');
+      result.innerHTML = `
+              <div class="card h-100">
+                <img src="${phones.data[i]?.image}" class="card-img-top w-75 m-auto mt-2" alt="image didn't find" />
+                <div class="card-body">
+                  <h5 class="card-title">${phones.data[i]?.phone_name}</h5>
+                  <h6 class="card-title">${phones.data[i]?.brand}</h6>
+                </div>
               </div>
-            </div>
-    `
-    resultContainer.appendChild(result);
+      `
+      resultContainer.appendChild(result);
+    }
   }
 }
