eb320f9e7f005f16d7a8b55bbb9d72fad3222f71
checkiamsiam <issiam02415@gmail.com>
2022-02-28 22:55:41 +0600

    somthing complite

---
+++ app.js
@@ -0,0 +1,29 @@
+const searchText = document.getElementById('search-field');
+const resultContainer = document.getElementById('result-container');
+
+
+document.getElementById('button-addon2').addEventListener('click', function () {
+  fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText.value}`)
+    .then(res => res.json())
+    .then(data => displayResult(data))
+})
+
+const displayResult = phones => {
+  searchText.value = '';
+  resultContainer.textContent = '';
+  for (let i = 0 ; i < 20 ; i++){
+    console.log(phones.data[i]);
+    const result = document.createElement('div');
+    result.classList.add('col');
+    result.innerHTML = `
+            <div class="card h-100">
+              <img src="${phones.data[i].image}" class="card-img-top w-75 m-auto mt-2" alt="image didn't find" />
+              <div class="card-body">
+                <h5 class="card-title">${phones.data[i].phone_name}</h5>
+                <h6 class="card-title">${phones.data[i].brand}</h6>
+              </div>
+            </div>
+    `
+    resultContainer.appendChild(result);
+  }
+}
